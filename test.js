a1d4a087e32a395eed2ec1f9b46e6a7da4159c3b
Pathos <shinigamiking14489@gmail.com>
2021-06-04 09:56:29 -0700

    changed from a named main() to an async wrapper

--- test.js
+++ test.js
@@ -2,13 +2,11 @@ import HorniClient from './index.js'
 
 const client = new HorniClient()
 
-async function main() {
+(async()=>{
     console.log(`Running Horni Test...\n`);
     var input       = `This is a`
     var generation  = await client.sendPrompt(input)
     var tokens      = generation.prompt.tokens.length+generation.results[0].tokens.length
     var output      = `${generation.total_elapsed_time}ms | ${tokens} Tokens w/prompt | ${input}${generation.results[0].content}`
     console.log(output);
-}
-
-main().then()
+})
