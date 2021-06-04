bb7110f4db1a4d0575190034be1ab316f9824433
Pathos <shinigamiking14489@gmail.com>
2021-06-04 13:55:22 -0700

    fixed test, apparently the async wrapper ain't it

--- test.js
+++ test.js
@@ -2,11 +2,13 @@ import HorniClient from './index.js'
 
 const client = new HorniClient()
 
-(async()=>{
+async function main() {
     console.log(`Running Horni Test...\n`);
     var input       = `This is a`
     var generation  = await client.sendPrompt(input)
     var tokens      = generation.prompt.tokens.length+generation.results[0].tokens.length
     var output      = `${generation.total_elapsed_time}ms | ${tokens} Tokens w/prompt | ${input}${generation.results[0].content}`
     console.log(output);
-})
+}
+
+main().then()
