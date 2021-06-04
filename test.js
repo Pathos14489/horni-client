27feb6fa95511f9eb85da4802dbe929a56613452
Pathos <shinigamiking14489@gmail.com>
2021-06-04 09:55:09 -0700

    updated test.js to work with latest API version

--- test.js
+++ test.js
@@ -3,14 +3,12 @@ import HorniClient from './index.js'
 const client = new HorniClient()
 
 async function main() {
-    var initialTimestamp = Date.now()
-    var output = `This is a${(await client.sendPrompt("This is a"))[0]}`
-    var generationTimestamp = Date.now()
-    var generationDifference = generationTimestamp-initialTimestamp
-    var tokens = (await client.getTokens(output)).length
-    var tokenTimestamp = Date.now()
-    var tokenDifference = tokenTimestamp-generationTimestamp
-    console.log(`Generation: ${output} (${generationDifference}ms) | Token Count: ${tokens} (${tokenDifference}ms)`)
+    console.log(`Running Horni Test...\n`);
+    var input       = `This is a`
+    var generation  = await client.sendPrompt(input)
+    var tokens      = generation.prompt.tokens.length+generation.results[0].tokens.length
+    var output      = `${generation.total_elapsed_time}ms | ${tokens} Tokens w/prompt | ${input}${generation.results[0].content}`
+    console.log(output);
 }
 
 main().then()
