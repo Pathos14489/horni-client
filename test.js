aece7437546c0e8d146e06ad90afd284190377b6
Pathos <shinigamiking14489@gmail.com>
2021-06-04 04:16:42 -0700

    Made test.js output more useful information

--- test.js
+++ test.js
@@ -3,7 +3,14 @@ import HorniClient from './index.js'
 const client = new HorniClient()
 
 async function main() {
-    console.log(await client.sendPrompt("This is a"))
+    var initialTimestamp = Date.now()
+    var output = `This is a${(await client.sendPrompt("This is a"))[0]}`
+    var generationTimestamp = Date.now()
+    var generationDifference = generationTimestamp-initialTimestamp
+    var tokens = (await client.getTokens(output)).length
+    var tokenTimestamp = Date.now()
+    var tokenDifference = tokenTimestamp-generationTimestamp
+    console.log(`Generation: ${output} (${generationDifference}ms) | Token Count: ${tokens} (${tokenDifference}ms)`)
 }
 
 main().then()
