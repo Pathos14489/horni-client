d56cd60289f7cd18e1559648646042e7609f7763
Pathos <shinigamiking14489@gmail.com>
2021-06-05 04:47:11 -0700

    removed dev log

--- metrics.js
+++ metrics.js
@@ -11,11 +11,11 @@ async function main() {
     for (let index = 0; index < testCount; index++) tests.push(client.sendPrompt(input)) // Adds tests to list.
     var completedTests      = await Promise.all(tests)
     var clientTimeElapsed   = Date.now()-startTime
-    console.log(completedTests[0]);
     var serverTime          = completedTests.map((a)=>a.total_elapsed_time)
     var totalServerTime     = serverTime.reduce((a,b)=>a+b)
     var testResults         = completedTests.map(test=>input+test.results[0].content)
-    var output              = `Tokens Per Request: ${client.default_prompt_settings.number_generated_tokens} - Request Count: ${testCount+1} - Server Generation Time: ${totalServerTime}ms - Client Start-to-Finish Time: ${clientTimeElapsed}ms}`
+    var output              = `Tokens Per Request: ${client.default_prompt_settings.number_generated_tokens} - Request Count: ${testCount+1} - Server Generation Time: ${totalServerTime}ms - Latency: ${`${clientTimeElapsed-totalServerTime}`.split(".")[0]}ms}` 
+    // Note, high latency is likely due to lazy queue checking for performance. The lazy queuechecker can be disabled and the checkQueue method can be called manually if you need to revise the queue to better suit your needs.
     console.log(output);
     console.log(testResults);
 }
