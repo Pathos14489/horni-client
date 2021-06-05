f31dbeb5e5ba290b39ff94c1b7db6cd4795e7d36
Pathos <shinigamiking14489@gmail.com>
2021-06-05 04:47:35 -0700

    Added Latency display

--- test.js
+++ test.js
@@ -4,10 +4,12 @@ const client = new HorniClient()
 
 async function main() {
     console.log(`Running Horni Test...\n`);
+    var startTime           = Date.now()
     var input       = `This is a`
     var generation  = await client.sendPrompt(input)
     var tokens      = generation.prompt.tokens.length+generation.results[0].tokens.length
-    var output      = `${generation.total_elapsed_time}ms | ${tokens} Tokens w/prompt | ${input}${generation.results[0].content}`
+    var clientTimeElapsed   = Date.now()-startTime
+    var output      = `Server Generation Time: ${generation.total_elapsed_time}ms - Latency: ${`${clientTimeElapsed-generation.total_elapsed_time}`.split(".")[0]}ms - ${tokens} Tokens w/prompt - ${input}${generation.results[0].content}`
     console.log(output);
 }
 
